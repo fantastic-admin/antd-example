efb64681a7437606a43517aae5d8b0557b5098df
Hooray Hu <304327508@qq.com>
2023-12-21 00:41:25 +0800

    迁移到 4.3.0

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import '@/utils/baidu'
 
 import '@/utils/system.copyright'
 
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/reset.css'
-
 import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
 
@@ -16,6 +13,7 @@ import 'overlayscrollbars/overlayscrollbars.css'
 import App from './App.vue'
 import pinia from './store'
 import router from './router'
+import ui from './ui-provider'
 
 // 自定义指令
 import directive from '@/utils/directive'
@@ -33,13 +31,13 @@ import 'virtual:uno.css'
 import '@/assets/styles/globals.scss'
 
 const app = createApp(App)
-app.use(Antd)
 app.use(FloatingVue, {
   distance: 12,
 })
 app.use(Message)
 app.use(pinia)
 app.use(router)
+app.use(ui)
 directive(app)
 if (icons.isOfflineUse) {
   for (const info of icons.collections) {
---
+++ src/ui-provider/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import antDesignVueLocaleZhCN from 'ant-design-vue/es/locale/zh_CN'
+import { theme } from 'ant-design-vue'
+import useSettingsStore from '@/store/modules/settings'
+
+const settingsStore = useSettingsStore()
+</script>
+
+<template>
+  <AConfigProvider :locale="antDesignVueLocaleZhCN" :theme="settingsStore.settings.app.colorScheme === 'dark' ? { algorithm: [theme.darkAlgorithm] } : {}">
+    <slot />
+  </AConfigProvider>
+</template>
