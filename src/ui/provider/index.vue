127f62e809a4f90ab6577f5edb26382019939075
Hooray <304327508@qq.com>
2025-10-28 16:19:42 +0800

    antdv 主题与框架主题同步

--- src/ui/provider/index.vue
+++ src/ui/provider/index.vue
@@ -5,8 +5,37 @@ import antDesignVueLocaleZhCN from 'ant-design-vue/es/locale/zh_CN'
 
 const settingsStore = useSettingsStore()
 
+const token = ref({
+  colorPrimary: '',
+  colorInfo: '',
+  colorTextBase: '',
+  colorBgElevated: '',
+  colorBgContainer: '',
+  colorBgBase: '',
+  borderRadius: 0,
+})
+
+watch([
+  () => settingsStore.settings.app.colorScheme,
+  () => settingsStore.settings.app.radius,
+], () => {
+  const rootStyles = getComputedStyle(document.documentElement)
+  token.value = {
+    colorPrimary: `hsl(${rootStyles.getPropertyValue('--primary')})`,
+    colorInfo: `hsl(${rootStyles.getPropertyValue('--primary')})`,
+    colorTextBase: `hsl(${rootStyles.getPropertyValue('--foreground')})`,
+    colorBgElevated: `hsl(${rootStyles.getPropertyValue('--popover')})`,
+    colorBgContainer: `hsl(${rootStyles.getPropertyValue('--card')})`,
+    colorBgBase: `hsl(${rootStyles.getPropertyValue('--background')})`,
+    borderRadius: Number.parseFloat(rootStyles.getPropertyValue('--radius')) * 16,
+  }
+}, {
+  immediate: true,
+})
+
 const themeConfig = computed(() => ({
   algorithm: settingsStore.currentColorScheme === 'dark' ? [theme.darkAlgorithm] : [theme.defaultAlgorithm],
+  token: token.value,
 }))
 </script>
 
